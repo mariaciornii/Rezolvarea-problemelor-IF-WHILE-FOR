79c2663e95d5aa2268ecd3f59ad7ca89ec7f9ff7
mariaciornii <70654766+mariaciornii@users.noreply.github.com>
2020-11-11 13:58:45 +0300

    Add files via upload

---
+++ Problema 1. WHILE FOR IF.py
@@ -0,0 +1,9 @@
+n=eval(input('n='))
+if (n==28) or (n==29):
+    print('luna este februarie')
+elif n==30:
+    print('lunile sunt aprilie, iunie, septembrie, noiembrie')
+elif n==31:
+    print('lunile sunt ianuarie, martie, mai, iulie, august, octombrie, decembrie')
+else: 
+    print('nu exista luni cu asa numarde zile')
